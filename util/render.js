b08c5e8c1dea9b7fdd3158676be52af8b07d2942
Celeite <contactceleite@gmail.com>
2024-06-14 13:06:48 -0700

    remove RenderLib

--- util/render.js
+++ util/render.js
@@ -1,9 +1,6 @@
 import settings from "../settings/settings";
 import BlingPlayer from "./BlingPlayer";
 import { rgbToColorInt } from "./helperFunctions";
-// i do not take credit for this code below in any way. I just needed to change a line for the purposes of this module.
-// original author of this method is Debuggings/Debug
-// here is a link to their module RenderLib: https://chattriggers.com/modules/v/Renderlib
 
 function drawWireBox(x, y, z, w, h, l, lWidth = 2.0, red, green, blue, alpha, phase) {
     if (settings().renderLimitEnabled && BlingPlayer.calcEyeDist(x, y, z) > settings().renderLimit * 16) {
